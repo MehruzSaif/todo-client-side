514738e21429c418881a72a9aa0441b2b1bf4d91
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-07-01 00:06:58 +0600

    added footer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 import Calender from './Pages/Calender/Calender';
 import Home from './Pages/Home/Home';
+import Completed from './Pages/Completed/Completed';
+import ToDo from './Pages/ToDo/ToDo';
 
 function App() {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto'>
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<Calender />} />
+        <Route path="completed" element={<Completed />} />
+        <Route path="toDo" element={<ToDo />} />
+        <Route path="calender" element={<Calender />} />
       </Routes>
     </div>
   );
---
+++ src/Pages/Completed/Completed.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Completed = () => {
+    return (
+        <div>
+            this is completed
+        </div>
+    );
+};
+
+export default Completed;
